6ad7d2f67ff3d58965076cb6cf010477d161e8c7
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2017-01-03 22:41:53 +0100

    Tested with Python 3.6

--- setup.py
+++ setup.py
@@ -37,5 +37,6 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
