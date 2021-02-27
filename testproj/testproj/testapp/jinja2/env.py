2566ba28b7647a38b0faccda310c479374888330
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-02-27 01:58:52 +0100

    Fix path to jinj2 tags in testproj

--- testproj/testproj/testapp/jinja2/env.py
+++ testproj/testproj/testapp/jinja2/env.py
@@ -1,7 +1,10 @@
 from jinja2.environment import Environment
 
 from django.template.defaultfilters import yesno
-from webstack_django_sorting.jinja2_globals import sorting_anchor, sort_queryset
+from webstack_django_sorting.templatetags.jinja2_globals import (
+    sorting_anchor,
+    sort_queryset,
+)
 
 
 class JinjaEnvironment(Environment):
