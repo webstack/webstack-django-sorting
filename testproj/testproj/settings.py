fe0b6e6ebb5b32b786480135dbd692f59146ad67
manderj <joffrey.mander@gmail.com>
2021-07-10 23:56:40 +0200

    tests: serve common templates css through static files

--- testproj/testproj/settings.py
+++ testproj/testproj/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = "static/"
 STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
