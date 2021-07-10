009a1c77026f24b0cadc50ffe1f44a03e165f68d
manderj <joffrey.mander@gmail.com>
2021-07-10 23:56:40 +0200

    tests: serve common templates css through static files

--- testproj/testproj/testapp/jinja2/env.py
+++ testproj/testproj/testapp/jinja2/env.py
@@ -1,6 +1,7 @@
 from jinja2.environment import Environment
 
 from django.template.defaultfilters import yesno
+from django.templatetags.static import static
 from webstack_django_sorting.jinja2_globals import (
     sorting_anchor,
     sort_queryset,
@@ -13,3 +14,4 @@ class JinjaEnvironment(Environment):
         self.filters["yesno"] = yesno
         self.globals["sorting_anchor"] = sorting_anchor
         self.globals["sort_queryset"] = sort_queryset
+        self.globals["static"] = static
