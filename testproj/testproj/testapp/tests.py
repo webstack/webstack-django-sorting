2e215964ac6b50beb1927e8aaab8fcc9af62fdc0
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2022-08-08 18:13:04 +0200

    Rewrite bad comparison of instances against strings
    - DRY on none files
    - no implicit casting of instances
    - support Django 4.1

--- testproj/testproj/testapp/tests.py
+++ testproj/testproj/testapp/tests.py
@@ -9,8 +9,12 @@ class IndexTest(TestCase):
     def setUp(self):
         self.url = reverse("secret_list")
 
-        models.SecretFile.objects.create(filename="foo.txt", order=1, size=1024)
-        models.SecretFile.objects.create(filename="bar.txt", order=2, size=512)
+        self.foo_file = models.SecretFile.objects.create(
+            filename="foo.txt", order=1, size=1024
+        )
+        self.bar_file = models.SecretFile.objects.create(
+            filename="bar.txt", order=2, size=512
+        )
 
     def test_list(self):
         response = self.client.get(self.url)
@@ -19,16 +23,16 @@ class IndexTest(TestCase):
 
     def test_sorting_direction(self):
         response = self.client.get(self.url, {"sort": "order", "dir": "asc"})
-        values = ["<SecretFile: #1 foo.txt>", "<SecretFile: #2 bar.txt>"]
+        values = [self.foo_file, self.bar_file]
         self.assertQuerysetEqual(response.context["secret_files"], values)
 
         response = self.client.get(self.url, {"sort": "order", "dir": "desc"})
-        values = ["<SecretFile: #2 bar.txt>", "<SecretFile: #1 foo.txt>"]
+        values = [self.bar_file, self.foo_file]
         self.assertQuerysetEqual(response.context["secret_files"], values)
 
         # Nothing wrong happens with an invalid direction and ASC is used
         response = self.client.get(self.url, {"sort": "order", "dir": "NOT"})
-        values = ["<SecretFile: #1 foo.txt>", "<SecretFile: #2 bar.txt>"]
+        values = [self.foo_file, self.bar_file]
         self.assertQuerysetEqual(response.context["secret_files"], values)
 
     def test_sorting_argument(self):
@@ -42,30 +46,28 @@ class NullsTestCase(TestCase):
         self.nulls_first_url = reverse("nulls_first")
         self.nulls_last_url = reverse("nulls_last")
 
-        models.SecretFile.objects.create(filename="foo.txt", order=1, size=1024)
-        models.SecretFile.objects.create(filename="bar.txt", order=2, size=512)
+        self.foo_file = models.SecretFile.objects.create(
+            filename="foo.txt", order=1, size=1024
+        )
+        self.bar_file = models.SecretFile.objects.create(
+            filename="bar.txt", order=2, size=512
+        )
+        self.none_file = models.SecretFile.objects.create(
+            filename=None, order=3, size=512
+        )
 
     def test_sorting_nulls_first(self):
         """Verify None sorted field_name is in first places when sorting in asc and desc order"""
 
-        models.SecretFile.objects.create(filename=None, order=3, size=512)
         # asc order
-        values = [
-            "<SecretFile: #3 None>",
-            "<SecretFile: #2 bar.txt>",
-            "<SecretFile: #1 foo.txt>",
-        ]
+        values = [self.none_file, self.bar_file, self.foo_file]
         response = self.client.get(
             self.nulls_first_url, {"sort": "filename", "nulls": "first", "dir": "asc"}
         )
         self.assertQuerysetEqual(list(response.context["secret_files"]), values)
 
         # desc order
-        values = [
-            "<SecretFile: #3 None>",
-            "<SecretFile: #1 foo.txt>",
-            "<SecretFile: #2 bar.txt>",
-        ]
+        values = [self.none_file, self.foo_file, self.bar_file]
         response = self.client.get(
             self.nulls_first_url,
             {"sort": "filename", "nulls": "first", "dir": "desc"},
@@ -75,24 +77,15 @@ class NullsTestCase(TestCase):
     def test_sorting_nulls_last(self):
         """Verify None sorted field_name is in last places when sorting in asc and desc order."""
 
-        models.SecretFile.objects.create(filename=None, order=3, size=512)
         # asc order
-        values = [
-            "<SecretFile: #2 bar.txt>",
-            "<SecretFile: #1 foo.txt>",
-            "<SecretFile: #3 None>",
-        ]
+        values = [self.bar_file, self.foo_file, self.none_file]
         response = self.client.get(
             self.nulls_last_url, {"sort": "filename", "nulls": "last", "dir": "asc"}
         )
         self.assertQuerysetEqual(list(response.context["secret_files"]), values)
 
         # desc order
-        values = [
-            "<SecretFile: #1 foo.txt>",
-            "<SecretFile: #2 bar.txt>",
-            "<SecretFile: #3 None>",
-        ]
+        values = [self.foo_file, self.bar_file, self.none_file]
         response = self.client.get(
             self.nulls_last_url, {"sort": "filename", "nulls": "last", "dir": "desc"}
         )
